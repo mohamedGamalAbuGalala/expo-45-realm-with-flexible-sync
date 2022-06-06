5d4ae183de44946417d13eb7d511027be344b0de
Robert Klubenspies <448701+rklubenspies@users.noreply.github.com>
2022-06-06 19:31:09 -0400

    Implement custom dev client

--- AppWrapper.tsx
+++ AppWrapper.tsx
@@ -1,3 +1,4 @@
+import 'expo-dev-client';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
@@ -5,7 +6,7 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
-export default function App() {
+export default function AppWrapper() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
